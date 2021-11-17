290b4aec551e8318e944ecf75853be613d0be6bb
ChocoDream <dh99@live.se>
2021-11-17 21:49:45 +0100

    Post to Server

--- backend/database/mongodb.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient } = require("mongodb");
-
-const uri =
-  "mongodb+srv://admin:<password>@personalstuff.i38hd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useUnifiedTopology: true,
-});
-module.exports = class mongodb {};
--- backend/main.js
+++ backend/main.js
@@ -6,12 +6,15 @@ const middleURL = "/api";
 
 //MONGODB
 const { MongoClient } = require("mongodb");
+const { text } = require("express");
 const uri =
   process.env.MONGODB_URI ||
   "mongodb+srv://admin:fx3VAXbWvRFuq1Cu@cluster0.i38hd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const dbName = "factsdb";
 
 app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
@@ -38,6 +41,30 @@ app.get(middleURL + "/fact", async (req, res) => {
     res.send(document[0]);
   } catch (error) {
     console.error(error);
+  } finally {
+    await client.close();
+  }
+});
+
+app.post(middleURL + "/fact", async (req, res) => {
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  try {
+    const request = await req;
+    if (!request.body || !request.body.text || request.body.text.length === 0)
+      throw "Empty body";
+    await client.connect();
+    const { text, author } = req.body;
+    const body = { text, author: author ?? "Unknown" };
+    const db = client.db(dbName);
+    const coll = db.collection("facts");
+    await coll.insertOne(body);
+    res.status(301).send({ status: "success", info: body });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ status: "failure", info: error });
   } finally {
     await client.close();
   }
