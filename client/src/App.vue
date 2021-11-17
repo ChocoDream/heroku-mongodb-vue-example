c6197efe166d7d835998aeb66ec10cad659920af
ChocoDream <dh99@live.se>
2021-11-17 10:27:45 +0100

    removing submodule

---
+++ client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app-wrapper">
+    <h1>Hello from Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+}
+
+
+.app-wrapper{
+  background-color: rgb(27, 27, 27);
+  color: aliceblue;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
