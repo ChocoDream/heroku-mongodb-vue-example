786ad061d35ab0ef888b6ada242e7d9edfa64905
Daniel <dh99@live.se>
2021-11-17 13:30:08 +0100

    Build client and move build to server
    Changed package.json to make a copy of the built client and paste into backend/public. After that's done, delete all files in client/build to reduce redundant code. backend/dist is ignored by git.

--- backend/main.js
+++ backend/main.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
-const apimiddle = "/api";
 
 const randomFacts = ["moo", "moooo", "I'm a dog bark bark", "bark", "baaaark"];
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/test.html"));
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 app.listen(port, () => {
