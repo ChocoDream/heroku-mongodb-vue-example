35b073f6eb515d30051fcd92d5fd91b13fd7dda8
Daniel <dh99@live.se>
2021-11-17 12:54:13 +0100

    Hot reload, speeds up build, development time + quick reload on CSS change

--- client/src/index.js
+++ client/src/index.js
@@ -2,3 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 createApp(App).mount("#root");
+
+if (import.meta.hot) {
+  import.meta.hot.accept();
+  import.meta.hot.dispose(() => {
+    app.unmount();
+  });
+}
