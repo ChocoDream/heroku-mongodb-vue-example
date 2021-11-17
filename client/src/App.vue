579af3728d4793a6f7d6812be44758a40ac97a53
Daniel <dh99@live.se>
2021-11-17 14:16:56 +0100

    Removed Counter

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,14 @@
 <template>
   <div class="app-wrapper">
-    <h1>Hello from Vue! {{ count }} seconds since app started</h1>
+    <h1>Hello from Vue!</h1>
     <random-fact />
   </div>
 </template>
 
 <script>
-import { onMounted, ref } from "vue";
 import RandomFact from "./Components/RandomFact.vue";
 export default {
   components: { RandomFact },
-  setup() {
-    const count = ref(0);
-
-    onMounted(() => {
-      const timer = setInterval(() => count.value++, 1000);
-      return () => {
-        clearInterval(timer);
-      };
-    });
-
-    return {
-      count,
-    };
-  },
 };
 </script>
 
